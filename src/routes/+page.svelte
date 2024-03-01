4afc7003d1130a48481890aad9f7544f4be443f3
André devkcud Albanese <patommmmm@proton.me>
2024-03-01 10:28:51 -0300

    Add interaction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,8 +10,12 @@
     aliases?: string[];
   };
 
+  const allIcons: { [key: string]: IconData } = data;
+
+  const githubUrl = 'https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/';
+
   let search = '';
-  $: filteredIcons = Object.entries(data)
+  $: filteredIcons = Object.entries(allIcons)
     .filter(([key, value]) =>
       search === ''
         ? true
@@ -23,7 +27,7 @@
     )
     .map(([key, value]) => [key, (value as IconData).name]);
 
-  let icons: string[] = []; // Placeholder
+  let icons: string[] = [];
   let lightMode: boolean = false;
   $: url = {
     base: 'https://skillicons.dev/icons',
@@ -46,26 +50,19 @@
 
     toast.push('Copied to clipboard');
   }
+
+  function toggleIcon(icon: string) {
+    if (icons.includes(icon)) {
+      icons = icons.filter((i) => i !== icon);
+    } else {
+      icons = [...icons, icon];
+    }
+  }
 </script>
 
 <svelte:head>
   <title>Skill Icons - Image Builder</title>
 </svelte:head>
-
-<input type="text" bind:value={search} placeholder="Search by name" class="block mx-auto input input-bordered input-sm w-full max-w-xs" />
-
-<div class="flex flex-wrap justify-center gap-4 m-4">
-  {#each filteredIcons as [entry, name]}
-    <img
-      src={`https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/${name}${!blacklist.includes(name) ? '-Dark' : ''}.svg`}
-      alt={name}
-      class={twMerge(
-        'size-12',
-        icons.includes(entry) ? 'rounded-[10px] ring-[8px] ring-offset-2 ring-red-500' : ''
-      )}
-    />
-  {/each}
-</div>
 
 <div class="flex flex-col items-center gap-4">
   <button on:click={toggleMode} class="btn btn-sm btn-ghost">
@@ -77,11 +74,56 @@
     {lightMode ? 'Light' : 'Dark'}
   </button>
 
-  <img src={formatUrl()} alt="Skill Icons" class="h-12" />
+  <div class="h-12 flex items-center">
+    {#if icons.length !== 0}
+      <div class="flex gap-2">
+        {#each icons as icon}
+          <div class="tooltip" data-tip={allIcons[icon].name}>
+            <button on:click={() => toggleIcon(icon)}>
+              <img
+                src={`${githubUrl}${allIcons[icon].name}${
+                  !blacklist.includes(allIcons[icon].name) ? (lightMode ? '-Light' : '-Dark') : ''
+                }.svg`}
+                alt={allIcons[icon].name}
+                class="size-12"
+              />
+            </button>
+          </div>
+        {/each}
+      </div>
+    {:else}
+      <p class="text-center">Click the icons below to add to the stack.</p>
+    {/if}
+  </div>
+
   <div class="flex gap-2 items-center">
     <span class="font-mono text-sm text-gray-500 bg-gray-200 p-2">{formatUrl(false)}</span>
     <button on:click={copyToClipboard}>
       <iconify-icon icon="lucide:clipboard-copy" class="text-neutral" />
     </button>
   </div>
+</div>
+
+<input
+  type="text"
+  bind:value={search}
+  placeholder="Search by name"
+  class="block mx-auto input input-bordered input-sm w-full max-w-xs mt-8"
+/>
+
+<div class="flex flex-wrap justify-center gap-4 m-4">
+  {#each filteredIcons as [entry, name]}
+    <div class="tooltip" data-tip={name}>
+      <button on:click={() => toggleIcon(entry)}>
+        <img
+          src={`${githubUrl}${name}${!blacklist.includes(name) ? '-Dark' : ''}.svg`}
+          alt={name}
+          class={twMerge(
+            'size-12',
+            icons.includes(entry) ? 'rounded-[10px] ring-[8px] ring-offset-2 ring-red-500' : ''
+          )}
+        />
+      </button>
+    </div>
+  {/each}
 </div>
