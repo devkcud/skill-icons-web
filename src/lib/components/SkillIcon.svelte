2ec12d0869e8b3924a71a0a8cfaae748031e1fa4
André devkcud Albanese <patommmmm@proton.me>
2024-03-01 23:10:14 -0300

    refactor: Code cleanup

---
+++ src/lib/components/SkillIcon.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import blacklist from '$lib/data/blacklist-lightmode.json';
+  import data from '$lib/data/icons.json';
+
+  export let id: string;
+  export let lightMode: boolean;
+  export let onClick: (...args: any) => void;
+
+  const githubUrl = 'https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/';
+
+  // @ts-ignore
+  $: name = data[id].name;
+  $: theme = !blacklist.includes(name) ? (lightMode ? '-Light' : '-Dark') : '';
+  // NOTE: This is a hacky way to get the icon name correctly
+  // In the original repo, the ScikitLearn icon is named like this: SciKitLearn-Light.svg or ScikitLearn-Dark.svg
+  // You might not see the diference, but the 'K' in 'kit' changes from 'K' to 'k' depending on the theme
+  $: iconName = (function () {
+    if (name === 'ScikitLearn') {
+      return theme === '-Light' ? 'SciKitLearn-Light.svg' : 'ScikitLearn-Dark.svg';
+    }
+
+    return `${name}${theme}.svg`;
+  })();
+  //$: iconName = `${name}${theme}.svg`; // If we lived in a perfect world
+</script>
+
+<div class="tooltip" data-tip={name}>
+  <button on:click={onClick}>
+    <img src={`${githubUrl}${iconName}`} alt={name} class="size-12" loading="lazy" />
+  </button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,29 @@
 <script lang="ts">
   import 'iconify-icon';
   import { toast } from '@zerodevx/svelte-toast';
-  import { twMerge } from 'tailwind-merge';
+  import SkillIcon from '$lib/components/SkillIcon.svelte';
   import data from '$lib/data/icons.json';
-  import blacklist from '$lib/data/blacklist-lightmode.json';
-
-  type IconData = {
-    name: string;
-    aliases?: string[];
-  };
-
-  const allIcons: { [key: string]: IconData } = data;
-
-  const githubUrl = 'https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/';
 
   let search = '';
-  $: filteredIcons = Object.entries(allIcons)
-    .filter(([key, value]) =>
-      search === ''
-        ? true
-        : key.toLowerCase().includes(search.toLowerCase()) ||
-          (value as IconData).name.toLowerCase().includes(search.toLowerCase()) ||
-          (value as IconData).aliases?.some((alias) =>
-            alias.toLowerCase().includes(search.toLowerCase())
-          )
-    )
-    .map(([key, value]) => [key, (value as IconData).name]);
+  $: filteredIcons = Object.entries(data)
+    .filter(function ([key, value]) {
+      if (search === '') {
+        return true;
+      }
+
+      return (
+        key.toLowerCase().includes(search.toLowerCase()) ||
+        value.name.toLowerCase().includes(search.toLowerCase()) ||
+        value.aliases?.some((alias) => alias.toLowerCase().includes(search.toLowerCase()))
+      );
+    })
+    .map(function ([key]) {
+      return key;
+    });
 
   let icons: string[] = [];
   let lightMode: boolean = false;
+
   $: url = {
     base: 'https://skillicons.dev/icons',
     icons: `?i=${icons.join(',')}`,
@@ -64,7 +59,7 @@
   <title>Skill Icons - Image Builder</title>
 </svelte:head>
 
-<div class="flex flex-col items-center gap-4">
+<section class="flex flex-col items-center gap-4">
   <button on:click={toggleMode} class="btn btn-sm btn-ghost">
     {#if lightMode}
       <iconify-icon icon="lucide:sun" />
@@ -77,18 +72,8 @@
   <div class="h-12 flex items-center">
     {#if icons.length !== 0}
       <div class="flex gap-2">
-        {#each icons as icon}
-          <div class="tooltip" data-tip={allIcons[icon].name}>
-            <button on:click={() => toggleIcon(icon)}>
-              <img
-                src={`${githubUrl}${allIcons[icon].name}${
-                  !blacklist.includes(allIcons[icon].name) ? (lightMode ? '-Light' : '-Dark') : ''
-                }.svg`}
-                alt={allIcons[icon].name}
-                class="size-12"
-              />
-            </button>
-          </div>
+        {#each icons as id (id)}
+          <SkillIcon {id} {lightMode} onClick={() => toggleIcon(id)} />
         {/each}
       </div>
     {:else}
@@ -102,28 +87,21 @@
       <iconify-icon icon="lucide:clipboard-copy" class="text-neutral" />
     </button>
   </div>
-</div>
+</section>
 
-<input
-  type="text"
-  bind:value={search}
-  placeholder="Search by name"
-  class="block mx-auto input input-bordered input-sm w-full max-w-xs mt-8"
-/>
+<section>
+  <div class="flex items-center justify-center mt-8 gap-2">
+    <input
+      type="text"
+      bind:value={search}
+      placeholder="Search by name"
+      class="input input-bordered input-sm"
+    />
+  </div>
 
-<div class="flex flex-wrap justify-center gap-4 m-4">
-  {#each filteredIcons as [entry, name]}
-    <div class="tooltip" data-tip={name}>
-      <button on:click={() => toggleIcon(entry)}>
-        <img
-          src={`${githubUrl}${name}${!blacklist.includes(name) ? '-Dark' : ''}.svg`}
-          alt={name}
-          class={twMerge(
-            'size-12',
-            icons.includes(entry) ? 'rounded-[10px] ring-[8px] ring-offset-2 ring-red-500' : ''
-          )}
-        />
-      </button>
-    </div>
-  {/each}
-</div>
+  <div class="flex flex-wrap justify-center gap-4 p-4 max-w-7xl mx-auto">
+    {#each filteredIcons as id (id)}
+      <SkillIcon {id} {lightMode} onClick={() => toggleIcon(id)} />
+    {/each}
+  </div>
+</section>
