81c890c5c53710423821d3f8edcf07f4971337fa
André devkcud Albanese Junior <patommmmm@proton.me>
2025-07-29 23:11:44 -0300

    chore: add better sanitization for perline value

--- src/lib/stores/perline.ts
+++ src/lib/stores/perline.ts
@@ -17,13 +17,21 @@ export const perline: Writable<number> = writable<number>(PERLINE.defaultValue);
  * It enforces boundaries on the input amount, ensuring it falls between 1 and 50, defaulting to 15 if the input is not a number.
  * The new value is both saved to the perline store and persisted in local storage.
  *
- * @param {number} amount - The new 'per line' value to be set, must be a number. **MIN: 1 | MAX: 50**
+ * @param {unknown} amount - The new 'per line' value to be set. **MIN: 1 | MAX: 50**
  */
-export function setPerline(amount: number): void {
-  if (isNaN(amount)) amount = 15;
-  if (amount < 1) amount = 1;
-  if (amount > 50) amount = 50;
+export function setPerline(amount: unknown): void {
+  let sanitizedAmount = sanitizePerline(amount);
 
-  perline.set(Number(amount));
-  storage.set(String(amount));
+  perline.set(sanitizedAmount);
+  storage.set(String(sanitizedAmount));
+}
+
+export function sanitizePerline(amount: unknown): number {
+  let sanitizedAmount = parseInt(String(amount).trim(), 10);
+
+  if (isNaN(sanitizedAmount)) sanitizedAmount = 15;
+  if (sanitizedAmount < 1) sanitizedAmount = 1;
+  if (sanitizedAmount > 50) sanitizedAmount = 50;
+
+  return sanitizedAmount;
 }
